c7d3c5b6c7c5e5774637d271b2e2ae481b2f163f
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2025-12-28 11:11:24 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 
+function makeTeam(id, name, color) {
+  let team = Team.all[id];
+  team.name = name;
+  team.color.set(color);
+  team.id = id;
+  team.setPalette(color);
+};
+
+const someTeam = makeTeam(5, "Grenarc", "5D964EFF");
