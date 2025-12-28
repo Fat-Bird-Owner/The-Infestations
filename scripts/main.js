c566dc8c34c1186113e2e6db08ba90c47112d780
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2025-12-28 11:41:49 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
-
+Events.on(ContentInitEvent, e => { 
 
 Object.keys(Blocks).forEach(function(block){
 if (Blocks[block] != null){
 Blocks[block].researchCostMultiplier = 0,
 Blocks[block].unlock()
 }
+});
+
 });
 
