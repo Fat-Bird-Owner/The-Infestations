c74597fbddd77c0d0e96d2482fb83db1e6831784
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2025-12-28 12:18:20 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,1 @@
-Events.on(WithdrawEvent, event => {
-    
-    if (Vars.headless) return;
-    
-    const build = event.build;
-    const item = event.item;
-    const amount = event.amount;
 
-    const text = "Taken " + amount + " " + item.localizedName + " from " + build.block.localizedName;
-
-    Vars.ui.hudfrag.showToast(text);
-});
