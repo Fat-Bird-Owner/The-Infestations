3cf8aafd7612dadab388daf78591b82b442afbc7
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2025-12-29 19:12:40 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
+var copWall = Blocks.copperWall;
 
+copperWall.buildType = () => extend(Wall.WallBuild, copWall,{
+// update
+update(){
+var hp = this.health;
+  
+if (hp === null) return;
+var postHp = hp - 1;
+this.health = postHp;
+
+if (postHp <= 0){
+this.kill();
+}
+
+}
+
+})
