9dd5efeb0d9a243b689843b803f1125e9dc9bc5c
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2025-12-29 20:02:52 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,1 @@
-// translation sake
-const infestation = Vars.mods.locateMod("infestation").meta;
-
-infestation.displayName = Core.bundle.get("mod.infestation.name");
-infestation.author = Core.bundle.get("mod.infestation.author");
-infestation.description = Core.bundle.get("mod.infestation.description");
-
-infestation.subtitle = Core.bundle.get("mod.infestation.subtitle");
+require("block");
