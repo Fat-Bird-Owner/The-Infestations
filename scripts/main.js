ae185b24c7cecc0184d694f6e8ae98a19d0e05b0
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2025-12-29 19:21:54 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,9 @@
 var copWall = Blocks.copperWall;
 
-copWall.update = true;
 copWall.buildType = () => extend(Wall.WallBuild, copWall,{
 // update
-update(){
-this.health -= 1;
-
-if (this.health <= 0){
-this.kill();
-}
-
+placed(){
+Vars.ui.hudfrag.showToast("Pathetic.");
 }
 
 })
