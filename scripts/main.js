1aae0f6b0b67947248904e98d37a3da4d4b208c9
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2025-12-29 17:33:58 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
+Events.on(PlayerChatEvent, e => {
+    if (!e.message || !e.message.startsWith("/")) return;
+    
+    var parts = e.message.split(" ");
+    var cmd = parts[0];
 
+if (cmd = "/su"){
+var px = Vars.player.x;
+var py = Vars.player.y;
+
+var unitType = parts[1]
+var unit = Vars.content.unit(unitType);
+
+if (!unit){
+Vars.ui.hudfrag.showToast("[red]Not a unit.");
+return;
+};
+
+unit.spawn(Vars.player.team, px, py);
+  
+};
+
+}
