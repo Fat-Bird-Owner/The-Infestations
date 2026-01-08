2a9b4aaba382a522356627efe9149a1e0cb86ec7
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-01-08 20:42:03 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 Events.on(UnitSpawnEvent, event => {
 
-    if(event.unit.type.name == "infestation-convoy"){
+    if(event.unit.type == "infestation-convoy"){
       Vars.ui.menus.announce("[orange]It's Here.");
         }
     }
